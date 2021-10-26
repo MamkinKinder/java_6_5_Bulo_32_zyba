66a464113b819062a3e738f85d79a0c7991f242a
MamkinKinder <alex.fedkiv@gmail.com>
2021-10-26 13:38:01 +0300

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -5,7 +5,17 @@ public class Main {
     public static void main(String[] args) {
         int toothCounter = 32;
 
-        //напишите тут ваш код
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+       
+
 
 
         System.out.println(toothCounter);
